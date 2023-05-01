6be1d06cf3d4d127b613c7c7f968fb540c19601c
alex2 <alex20jiang07@gmail.com>
2023-05-01 20:53:50 +0200

    Hihihiha

--- src/main/java/my_project/model/Brett.java
+++ src/main/java/my_project/model/Brett.java
@@ -1,7 +1,21 @@
 package my_project.model;
 
-public class Brett {
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+import java.awt.*;
+
+public class Brett extends GraphicalObject {
     public Brett(){
 
+    }
+
+    public void draw(DrawTool drawTool){
+        drawTool.setCurrentColor(new Color(1,1,1));
+        for(int i = 0; i < 12; i++){
+            for(int j = 0; j < 12; j = j +2){
+                drawTool.drawFilledRectangle(x + 50*j + 50* (i % 2) ,y+i*50,50,50);
+            }
+        }
     }
 }
